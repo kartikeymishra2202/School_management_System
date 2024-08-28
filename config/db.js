7ac5ff286d7bdd4002956efecc33ecf2b93f94ce
kartikeymishra2202 <mharmilap@gmail.com>
2024-08-28 07:34:31 +0530

    add some changes to database

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const connection = mysql.createConnection(urlDB);
 
 db.connect((err) => {
   if (err) {
